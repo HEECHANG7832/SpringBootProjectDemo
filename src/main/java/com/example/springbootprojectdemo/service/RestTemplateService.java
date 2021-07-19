ba657df147a3f2424e47d31da0eb9557b83f2e76
HEECHANG7832 <facee22av@naver.com>
2021-07-19 10:56:56 +0900

    RestTemplate post

--- src/main/java/com/example/springbootprojectdemo/service/RestTemplateService.java
+++ src/main/java/com/example/springbootprojectdemo/service/RestTemplateService.java
@@ -1,5 +1,6 @@
 package com.example.springbootprojectdemo.service;
 
+import com.example.springbootprojectdemo.dto.UserRequest;
 import com.example.springbootprojectdemo.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ public class RestTemplateService {
 
     public  UserResponse hello(){
         URI uri = UriComponentsBuilder
-                .fromUriString("http://localhost")
+                .fromUriString("http://localhost:9090")
                 .path("api/server/hello")
                 .queryParam("name", "steve")
                 .queryParam("age", 10)
@@ -34,5 +35,29 @@ public class RestTemplateService {
         System.out.println(result.getBody());
 
         return result.getBody();
+    }
+
+    public UserResponse post(){
+        //http://ocalhost:9090/api/server/user/{userId}/name/{userName}
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+
+        //http body -> object -> object mapper -> json -> resttemplate -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        response.getStatusCode();
+        response.getHeaders();
+
+        return response.getBody();
     }
 }
